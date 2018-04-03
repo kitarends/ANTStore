aa66a6e09c1b414aab7e8fab4e422c3fd3f5043c
Thang Phi <thangpmedia@gmail.com>
2018-04-04 02:00:50 +0700

    fix product table display

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -12,7 +12,7 @@
         <div class="ui huge header">Products</div>
         <a class="ui primary button" href="/products/create" style="margin-bottom: 10px"><i class="plus icon"></i>Create new product</a>
 
-        <table class="ui celled table" id="products_table" >
+        <table class="ui celled table" >
             <thead>
             <tr>
                 <th>Image</th>
