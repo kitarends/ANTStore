3e44ea0972fb46d74aca5952dbb8a75e36c4dbea
Thang Phi <thangpmedia@gmail.com>
2018-04-04 01:23:26 +0700

    test update

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="ui container">
     <img src="/images/banner.jpg" style="width: 100%">
-    <div class="ui huge header" style="position: absolute; top: 50%;left: 30%;color: white  ;">Welcome to our store
+    <div class="ui huge header" style="position: absolute; top: 53%;left: 30%;color: white  ;">Welcome to our store, enjoy shopping
     </div>
     <div class="ui divider"></div>
 
