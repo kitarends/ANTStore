5c92204faee663b046eaf8eb35969ea29b9b9023
kitarends <thangpmedia@gmail.com>
2018-05-11 00:37:22 +0700

    hide old value

--- resources/views/ui/form/image.blade.php
+++ resources/views/ui/form/image.blade.php
@@ -2,7 +2,7 @@
     <label for="{{$name}}">{{$label}}</label>
     <input name="{{$name}}" type="file">
     @if(isset($item[$name]))
-        <input type="text" value="{{$item[$name]}}" name="{{$name}}_old">
+        <input type="hidden" value="{{$item[$name]}}" name="{{$name}}_old">
         <label style="margin-top: 10px">{{$label}}'s old value</label>
         <img src="/images/{{$item[$name]}}" style="max-height: 300px;margin-top: 4px">
     @endif
